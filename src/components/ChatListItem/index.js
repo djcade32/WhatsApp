6e37f205887170923352b9cf8bfe6267861d0046
Norman Cade <norman_cade32@yahoo.com>
2022-10-21 22:12:34 -0400

    ChatList rendered with dynamic data

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,4 +1,7 @@
 import { Text, View, Image, StyleSheet } from "react-native";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 function ChatListItem({ chat }) {
   return (
@@ -14,7 +17,9 @@ function ChatListItem({ chat }) {
           <Text numberOfLines={1} style={styles.name}>
             {chat.user.name}
           </Text>
-          <Text style={styles.subTitle}>{chat.lastMessage.createdAt}</Text>
+          <Text style={styles.subTitle}>
+            {dayjs(chat.lastMessage.createdAt).fromNow(true)}
+          </Text>
         </View>
         <Text numberOfLines={2} style={styles.subTitle}>
           {chat.lastMessage.text}
---
+++ src/screens/ChatScreen.js
@@ -0,0 +1,13 @@
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import chats from "../../assets/data/chats.json";
+import ChatListItem from "../components/ChatListItem";
+
+export default function ChatScreen() {
+  return (
+    <FlatList
+      data={chats}
+      renderItem={({ item }) => <ChatListItem chat={item} />}
+    />
+  );
+}
