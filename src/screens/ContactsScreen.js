325cdbef8ab1adc67f3157d277edaa543a283aa6
Norman Cade <norman_cade32@yahoo.com>
2022-10-23 07:47:08 -0400

    Pulled user info for contact list

--- src/screens/ContactsScreen.js
+++ src/screens/ContactsScreen.js
@@ -1,13 +1,22 @@
 import { FlatList } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ContactListItem from "../components/ContactListItem";
-import chats from "../../assets/data/chats.json";
+import { API, graphqlOperation } from "aws-amplify";
+import { listUsers } from "../graphql/queries";
 
 export default function ContactsScreen() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    API.graphql(graphqlOperation(listUsers)).then((results) => {
+      console.log(results);
+      setUsers(results.data?.listUsers?.items);
+    });
+  }, []);
   return (
     <FlatList
-      data={chats}
-      renderItem={({ item }) => <ContactListItem user={item.user} />}
+      data={users}
+      renderItem={({ item }) => <ContactListItem user={item} />}
       style={{ backgroundColor: "white" }}
     />
   );
